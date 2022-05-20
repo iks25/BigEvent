aa04924fc26bc402b339e35c6a61ef2aed3d3e74
Igor Kozerski <ikspun@gmail.com>
2022-05-20 03:25:26 +0100

    adding create Event View

---
+++ BigEvent/Controllers/EventController.cs
@@ -0,0 +1,41 @@
+﻿using BigEvent.Data;
+using BigEvent.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BigEvent.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public EventController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            var listSelectedItems = new List<SelectListItem>();
+            _dbContext.EventTypes.ToList().ForEach(eventType =>
+            {
+                listSelectedItems
+                .Add(new SelectListItem()
+                {
+                    Value = eventType.Name,
+                    Text = eventType.Name
+                });
+
+            });
+            var viewItem = new EventViewModel() { Types = listSelectedItems };
+
+            return View(viewItem);
+        }
+    }
+}
---
+++ BigEvent/ViewModel/EventViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BigEvent.ViewModel
+{
+    public class EventViewModel
+    {
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Time { get; set; }
+        
+        public string Data { get; set; }
+        public List<SelectListItem> Types { get; set; }
+        public int EventType { get; set; }
+        public float TicketPrice { get; set; }
+    }
+}
