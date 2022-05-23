c3199d67c22ac53dd0993c34b5cf0d0e631ac686
Igor Kozerski <ikspun@gmail.com>
2022-05-24 00:30:13 +0100

    displaying list of events

---
+++ BigEvent/Controllers/EventsListController.cs
@@ -0,0 +1,35 @@
+﻿using BigEvent.Data;
+using BigEvent.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BigEvent.Controllers
+{
+    public class EventsListController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public EventsListController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var eventList = new List<EventsListViewModel>();
+
+            var eventListFromDb =
+                _dbContext.Events.Include(e => e.Organizer);
+
+            foreach (var item in eventListFromDb)
+            {
+                eventList.Add(new EventsListViewModel(item));
+            }
+
+
+
+            return View(eventList);
+        }
+    }
+}
---
+++ BigEvent/ViewModel/EventsListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BigEvent.Models;
+
+namespace BigEvent.ViewModel
+{
+    public class EventsListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string OrganizerName { get; set; }
+        public string Time { get; set; }
+        public string Date { get; set; }
+        public string DateOfTheWeek { get; set; }
+        public string HostName { get; set; }
+
+        public EventsListViewModel(Event eventEntity)
+        {
+            Id = eventEntity.Id;
+            Name = eventEntity.Name;
+            Time = eventEntity.DateTime.TimeOfDay.ToString();
+            Date = eventEntity.DateTime.Date.ToString();
+            DateOfTheWeek = eventEntity.DateTime.DayOfWeek.ToString();
+            HostName = eventEntity.Organizer.UserName;
+
+        }
+    }
+
+
+}
