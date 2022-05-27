461569fcd0e9a2430faeea78a1a742582219e5ed
Igor Kozerski <ikspun@gmail.com>
2022-05-27 02:54:32 +0100

    handle select picture look

--- BigEvent/wwwroot/js/site.js
+++ BigEvent/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/**
+ * 
+ * @param {HTMLDivElement} item
+ */
+function chooseImage(item) {
+    let divId = item.id;
+    let pictureId = item.getAttribute("data-pictureId");
+
+    changeSelectedImageDiv(item)
+    //TODO Change custom Input
+}
+/**
+ *
+ * @param {HTMLDivElement} selectedDiv
+ */
+function changeSelectedImageDiv(selectedDiv) {
+    let pictures = $(".gallery-picture-container");
+    pictures.removeClass("chosen-img");
+
+    selectedDiv.classList.add("chosen-img");
+}
+
+$(document).ready(() => {
+
+
+
+
+});
